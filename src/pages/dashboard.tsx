6e363ca766e19ddc515c7f897de98bf489e1ee23
Jin Bok de Oliveira <jinoliveira74@gmail.com>
2021-05-12 09:56:10 -0300

    chore: link

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -61,7 +61,7 @@ const series = [
 
 export default function Dashboard() {
   return (
-    <Flex direction="column" h="100vh">
+    <Flex direction="column" h="100vh"> 
       <Header />
 
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
