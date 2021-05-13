ab753aed5e563f4bef212d2f8bcbd1bb059ab4fc
Jin Bok de Oliveira <jinoliveira74@gmail.com>
2021-05-13 18:57:44 -0300

    chore: form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import { Flex, Button, Stack, } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from '../components/Form/Input'
 
+type SigInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+
+  const { register, handleSubmit, formState } = useForm()
+
+  const { errors } = formState
+
+  const handleSignIn: SubmitHandler<SigInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    
+    console.log(values)
+  }
+
   return (
     <Flex
       w="100vw"
@@ -17,6 +34,7 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
           
@@ -24,12 +42,14 @@ export default function SignIn() {
             name="email"
             type="email"
             label="E-mail"
+            {...register('email')}
           />
           
           <Input
             name="password"
             type="password"
             label="Senha"
+            {...register('password')}
           />
 
         </Stack>
@@ -39,6 +59,7 @@ export default function SignIn() {
           mt="6"
           colorScheme="pink"
           size="lg"
+          isLoading={formState.isSubmitting}
         >
           Entrar
         </Button>
